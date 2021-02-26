c9ecdd360c3d7381c142a17e19c93f0d1ef16a25
andrii_bublyk <andriy.bublyk1991@gmail.com>
2021-02-26 09:09:14 +0200

    added server var

--- app.py
+++ app.py
@@ -59,6 +59,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 # app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+server = app.server
+
 stocks = []
 crypto = []
 hryvnia = []
